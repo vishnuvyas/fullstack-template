a8cb6d6678a2286ce4aa7971f975a4cb43951306
vishnuvyas sethumadhavan <vishnuvyas@gmail.com>
2023-12-13 22:16:17 -0800

    added static files to have html=True

--- app-backend/app_backend/restapi/__init__.py
+++ app-backend/app_backend/restapi/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+BASE_PATH = "/api/v2"
 api = FastAPI()
-api.mount("/static", StaticFiles(directory="static"), name='static')
+api.mount(
+    "/static", StaticFiles(directory='static', html=True), name='static')
 
 
 @api.get("/health")
